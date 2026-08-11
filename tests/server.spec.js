af288b82e34631c709974d03447e93cefad01349
jeffrey-d-boyd <jeffrey.d.boyd.1@gmail.com>
2026-08-11 11:56:11 -0700

    fixed test

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -43,7 +43,7 @@ describe('server/app.js', function() {
     .get('/')
     .end((err, res) => {
       expect(err).not.exist;
-      expect(JSON.stringify(res.text)).to.contain("You're serving up the public folder.");
+      expect(JSON.stringify(res.text)).to.contain("You're serving up the public folder, but do your tests pass?");
       done();
     });
   });
